69a473c5ab9e3c9cbc9ebf0ef2d7a2e770bed01e
Aisha-Sinha-Sita <Aisha.Sinha@sita.aero>
2025-09-01 07:14:29 +0000

    adding infrastructure

---
+++ main.tf
@@ -0,0 +1,41 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.43.0"
+    }
+  }
+  cloud {
+
+    organization = "Aisha-Sita"
+
+    workspaces {
+      name = "Hands-On_With_Terraform_On_Azure"
+    }
+  }
+}
+
+provider "azurerm" {
+ subscription_id = "28e1e42a-4438-4c30-9a5f-7d7b488fd883"
+  features {}
+  resource_provider_registrations = "none"
+}
+
+# Explicitly register required resource providers (recommended alternative to skip_provider_registration)
+resource "azurerm_resource_provider_registration" "example" {
+  name = "Microsoft.Resources"
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "813-dfe92732-hands-on-with-terraform-on-azure"
+  location = "South Central US"
+}
+
+module "securestorage" {
+  source  = "app.terraform.io/Aisha-Sita/securestorage/azurerm"
+  version = "1.0.0"
+  # insert required variables here
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_name = "AishaSinha15"
+}
